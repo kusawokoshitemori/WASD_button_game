3efa8226ad51a3f1ee6341e252172f090acb57ab
草をこして森 <kyou0317kyou0317@gmail.com>
2024-06-28 21:55:39 +0900

    色変える前のセーブ地点

--- click_game/click_game.js
+++ click_game/click_game.js
@@ -1,11 +1,33 @@
 let score = 0;
+let next = "W";
 const scoreDisplay = document.getElementById("score");
 
 // キーボードのキー押下時のイベントリスナー
 document.addEventListener("keydown", function (event) {
-  if (event.key === "w") {
-    score++;
+  if (event.key === "w" && next === "W") {
+    score += 0.25;
+    next = "A";
     scoreDisplay.textContent = `スコア: ${score}`;
-    console.log("w push");
+
+    //やってみた
+    documents.getElementById("W-Key-Display").color = "red";
+
+    // 他のキーに対する処理...
+  }
+  //すぺーーーす
+  if (event.key === "a" && next === "A") {
+    score += 0.25;
+    next = "S";
+    scoreDisplay.textContent = `スコア: ${score}`;
+  }
+  if (event.key === "s" && next === "S") {
+    score += 0.25;
+    next = "D";
+    scoreDisplay.textContent = `スコア: ${score}`;
+  }
+  if (event.key === "d" && next === "D") {
+    score += 0.25;
+    next = "W";
+    scoreDisplay.textContent = `スコア: ${score}`;
   }
 });
